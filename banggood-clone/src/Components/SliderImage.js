8b4a5013ad3de9923edede09769e5f6e5798bfc6
Pallav <Pallavaj72218@gmail.com>
2022-06-30 10:27:34 +0530

    home page done

---
+++ banggood-clone/src/Components/SliderImage.js
@@ -0,0 +1,19 @@
+export const SliderImage= [
+    {
+        "url": "https://imgaz.staticbg.com/banggood/os/202206/20220627033950_494.jpg"
+    },
+    {
+        "url": "https://imgaz.staticbg.com/banggood/os/202205/20220518012037_337.jpg"
+    },
+    {
+        "url": "https://imgaz.staticbg.com/banggood/os/202206/20220627033930_605.jpg"
+    },
+    {
+        "url": "https://imgaz.staticbg.com/banggood/os/202206/20220626213256_233.jpg"
+    },
+    {
+        "url": "https://imgaz.staticbg.com/banggood/os/202206/20220622033611_318.jpg"
+    }, {
+        "url": "https://imgaz.staticbg.com/banggood/os/202206/20220627035609_672.jpg"
+    }
+]
